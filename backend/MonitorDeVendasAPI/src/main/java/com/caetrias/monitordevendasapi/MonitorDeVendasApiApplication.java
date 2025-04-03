5fef38ada8d70f6a0544ce9081b03b98c4216834
Gabriel Caetano Farias <gabrielcfarias77@gmail.com>
2025-04-02 22:41:08 -0300

    Adicionada base do projeto API Monitor de Vendas

---
+++ backend/MonitorDeVendasAPI/src/main/java/com/caetrias/monitordevendasapi/MonitorDeVendasApiApplication.java
@@ -0,0 +1,13 @@
+package com.caetrias.monitordevendasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorDeVendasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorDeVendasApiApplication.class, args);
+    }
+
+}
---
+++ backend/MonitorDeVendasAPI/src/test/java/com/caetrias/monitordevendasapi/MonitorDeVendasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caetrias.monitordevendasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorDeVendasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
